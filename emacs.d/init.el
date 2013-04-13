ea8abd84cee6dedb05b370dae0a0053db9052ada
Jez Cope <J.Cope@bath.ac.uk>
2013-04-13 09:21:52 +0100

    Add el-get config to init.el.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -17,6 +17,26 @@
   '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
+;; Set up el-get
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(setq el-get-sources
+  '((:name pandoc-mode
+       :type github
+       :description "Emacs mode for interacting with Pandoc."
+       :pkgname "joostkremers/pandoc-mode")))
+(setq my-packages
+  (append
+      '(el-get ack markdown-mode pandoc-mode)
+      (mapcar 'el-get-source-name el-get-sources)))
+(el-get 'sync my-packages)
+
 ;; Setup markdown-mode
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
