d9858039e600d170ab65002712137cf8e144ae2b
Jez Cope <j.cope@erambler.co.uk>
2013-04-13 12:21:45 +0100

    More emacs customisation.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,7 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-PDF-mode t))
+ '(TeX-PDF-mode t)
+ '(custom-safe-themes (quote ("d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" default)))
+ '(ecb-options-version "2.40")
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -27,15 +30,33 @@
     (eval-print-last-sexp)))
 
 (setq el-get-sources
-  '((:name pandoc-mode
-       :type github
-       :description "Emacs mode for interacting with Pandoc."
-       :pkgname "joostkremers/pandoc-mode")))
+      '(
+        (:name ecb
+	       :description "Emacs Code Browser"
+	       :type github
+	       :pkgname "alexott/ecb"
+	       ;:depends cedet
+	       :build `(("make" "CEDET=../cedet" ,(concat "EMACS=" (shell-quote-argument el-get-emacs)))))
+	(:name pandoc-mode
+	       :type github
+	       :description "Emacs mode for interacting with Pandoc."
+	       :pkgname "joostkremers/pandoc-mode")))
 (setq my-packages
   (append
-      '(el-get ack markdown-mode pandoc-mode magit)
+      '(el-get
+	;ecb
+	ack
+	markdown-mode
+	pandoc-mode
+	auctex
+	magit
+	zenburn-theme
+	)
       (mapcar 'el-get-source-name el-get-sources)))
 (el-get 'sync my-packages)
+
+;; Setup theme
+(load-theme 'tango-dark t)
 
 ;; Setup markdown-mode
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
