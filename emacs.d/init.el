0e5bb6be1945533d1fab6e4d59c74a1cac42b8a4
Jez Cope <J.Cope@bath.ac.uk>
2013-05-08 21:40:17 +0100

    More general emacs customisation.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4,8 +4,13 @@
 ;; General setup
 (require 'uniquify)
 (setq tool-bar-mode nil
+      menu-bar-mode nil
       uniquify-buffer-name-style (quote post-forward)
-      indent-tabs-mode nil)
+      indent-tabs-mode nil
+      standard-indent 2
+      ;; TODO: There must be a more concise way to express this:
+      tab-stop-list (quote (2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32))
+      tab-width 2)
 (global-auto-revert-mode)
 
 ;; Choose theme
@@ -24,7 +29,8 @@
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 (add-hook 'markdown-mode-hook 'turn-on-pandoc)
-(add-hook 'markdown-mode-hook 'visual-line-mode)
+;(add-hook 'markdown-mode-hook 'visual-line-mode)
+(add-hook 'markdown-mode-hook 'auto-fill-mode)
 (add-hook 'markdown-mode-hook 'flyspell-mode)
 
 ;; Setup for ruby
@@ -54,11 +60,15 @@
                    newline empty space-after-tab space-mark tab-mark
                    newline-mark)))
 
+;; Setup for shell scripts
+(add-to-list 'auto-mode-alist '("\\.zsh\\'" . sh-mode))
+
 ;; Load local settings
 (load "init-local" t)
 
 ;; Machine-local customizations
-(setq custom-file "~/.emacs.d/custom.el")
+(setq custom-file
+      (concat "~/.emacs.d/custom." (system-name) ".el"))
 (load custom-file t)
 
 ;; And finally start the server
