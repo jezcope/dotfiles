68f81610933616ba5a28bd2e719d3d05ba853136
Jez Cope <j.cope@erambler.co.uk>
2014-06-07 20:02:02 +0100

    Use emacsclient as default editor in awesome.

--- awesomerc.lua
+++ awesomerc.lua
@@ -47,8 +47,7 @@ beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "x-terminal-emulator"
-editor = os.getenv("EDITOR") or "editor"
-editor_cmd = terminal .. " -e " .. editor
+editor_cmd = "emacsclient -c"
 
 -- Default modkey.
 -- Usually, Mod4 is the key with a logo between Control and Alt.
