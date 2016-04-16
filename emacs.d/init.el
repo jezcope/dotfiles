e885ac4c4f3adc471480b75f94cbe53ba9f9927a
Jez Cope <j.cope@erambler.co.uk>
2016-04-16 20:03:22 +0100

    Initial switch to cask configuration

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,31 +3,35 @@
 ;; This file loads Org-mode and then loads the rest of our Emacs initialization from Emacs lisp
 ;; embedded in literate Org-mode files.
 
-;; Make dotfiles-dir point to "~/.emacs.d/"
-(setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+;; Make user-emacs-directory point to "~/.emacs.d/"
+(setq user-emacs-directory (file-name-directory (or (buffer-file-name) load-file-name)))
+
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+(require 'use-package)
 
 ;; Add ~/.emacs.d/elpa if it doesn't exist
-(make-directory (expand-file-name "elpa" dotfiles-dir) t)
+;; (make-directory (expand-file-name "elpa" user-emacs-directory) t)
 
 ;; Bootstrap el-get and org mode
-(load (expand-file-name "el-get-local.el" dotfiles-dir) t)
-(add-to-list 'load-path
-	     (expand-file-name "el-get" (if (boundp 'el-get-dir)
-					    el-get-dir
-					    "~/.emacs.d/el-get/")))
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (let (el-get-master-branch)
-      (goto-char (point-max))
-      (eval-print-last-sexp))))
-(el-get 'sync '(el-get org-mode)) ;; NB - need to install org-mode here!
+;; (load (expand-file-name "el-get-local.el" user-emacs-directory) t)
+;; (add-to-list 'load-path
+;; 	     (expand-file-name "el-get" (if (boundp 'el-get-dir)
+;; 					    el-get-dir
+;; 					    "~/.emacs.d/el-get/")))
+;; (unless (require 'el-get nil 'noerror)
+;;   (with-current-buffer
+;;       (url-retrieve-synchronously
+;;        "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+;;     (let (el-get-master-branch)
+;;       (goto-char (point-max))
+;;       (eval-print-last-sexp))))
+;; (el-get 'sync '(el-get org-mode)) ;; NB - need to install org-mode here!
 
 ;; Load up main init file
-(require 'org)
+(use-package org)
 
 (defun jc/require-org-file (name)
-  (org-babel-load-file (expand-file-name name dotfiles-dir)))
+  (org-babel-load-file (expand-file-name name user-emacs-directory)))
 
 (jc/require-org-file "init-main.org")
